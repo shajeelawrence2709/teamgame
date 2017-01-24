ddd915d6a77467f3427172a66780b3d5f02b7b70
Jesna <Jesna>
2017-01-23 23:15:58 -0600

    modified by jesna

--- src/com/A/A_One.java
+++ src/com/A/A_One.java
@@ -1,5 +1,8 @@
 package com.A;
 
 public class A_One {
+	public static void main(String[] args) {
+		
+	}
 
 }
--- src/com/B/JesnaTwo.java
+++ src/com/B/JesnaTwo.java
@@ -3,8 +3,7 @@ package com.B;
 public class JesnaTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("aaa");
 	}
 
 }
